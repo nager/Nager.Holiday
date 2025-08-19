d5436d740bb80a795ac33f660f8f930a6bbabe8a
Tino Hager <tino.hager@nager.at>
2025-08-19 17:12:22 +0200

    Optimize Holiday Model

--- src/Nager.Holiday/PublicHoliday.cs
+++ src/Nager.Holiday/PublicHoliday.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Nager.Holiday
 {
@@ -11,34 +11,37 @@ namespace Nager.Holiday
         /// The date
         /// </summary>
         public DateTime Date { get; set; }
+
         /// <summary>
         /// Local name
         /// </summary>
         public string? LocalName { get; set; }
+
         /// <summary>
         /// English name
         /// </summary>
         public string? Name { get; set; }
+
         /// <summary>
         /// ISO 3166-1 alpha-2
         /// </summary>
         public string? CountryCode { get; set; }
-        /// <summary>
-        /// Is this public holiday every year on the same date
-        /// </summary>
-        public bool Fixed { get; set; }
+
         /// <summary>
         /// Is this public holiday in every county (federal state)
         /// </summary>
         public bool Global { get; set; }
+
         /// <summary>
         /// ISO-3166-2 - Federal states
         /// </summary>
         public string[]? Counties { get; set; }
+
         /// <summary>
         /// A list of types the public holiday it is valid
         /// </summary>
         public PublicHolidayType[]? Types { get; set; }
+
         /// <summary>
         /// The launch year of the public holiday
         /// </summary>
